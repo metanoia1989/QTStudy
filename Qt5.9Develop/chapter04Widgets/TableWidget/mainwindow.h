fcc09ccd03577794c69fd0049e422ef83b667dd5
metanoia1989 <sogaxili@gmail.com>
2019-11-25 00:06:33 +0800

    添加 QTableWidget

--- Qt5.9Develop/chapter04Widgets/TableWidget/mainwindow.h
+++ Qt5.9Develop/chapter04Widgets/TableWidget/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QTableWidgetItem>
+#include "qwintspindelegate.h"
 
 namespace Ui {
 class MainWindow;
@@ -16,6 +19,21 @@ public:
     ~MainWindow();
 
 private:
+    // 为某一行创建 items
+    void createItemsARow(int rowNo, QString Name, QString Sex, QDate brith,
+                         QString Nation, bool isPM, int score);
+
+    // 自定义单元格Type的类型，在创建单元格的Item时使用
+    enum CellType{ctName=1000, ctSex, ctBirth, ctNation, ctPartyM, ctScore}; // 各单元格的类型
+    // 各字段在表格中的列号
+    enum FieldColNum{colName=0, colSex, colBirth, colNation, colScore, colPartyM};
+
+    QLabel* labCellIndex;	// 状态栏上用于显示单元格的行号、列号
+    QLabel* labCellType;	// 状态栏上用于显示单元格的type
+    QLabel* lablStudID;		// 状态栏上用于显示单元格的data
+
+    QWIntSpinDelegate spinDelegate;	// 代理组件
+
     Ui::MainWindow *ui;
 };
 
