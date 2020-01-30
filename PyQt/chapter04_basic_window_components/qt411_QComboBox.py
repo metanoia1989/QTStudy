059aaa2614ace4185386ee29fffb99f3619744d3
metanoia1989 <sogaxili@gmail.com>
2020-01-30 16:05:37 +0800

    添加 QComboBox

---
+++ PyQt/chapter04_basic_window_components/qt411_QComboBox.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class ComboxDemo(QWidget):
+    def __init__(self, parent=None):
+        super(ComboxDemo, self).__init__(parent)
+        self.setWindowTitle("combox 例子")
+        self.resize(300, 90)
+
+        layout = QVBoxLayout()
+        self.lbl = QLabel("")
+
+        self.cb = QComboBox()
+        self.cb.addItem("C")
+        self.cb.addItem("C++")
+        self.cb.addItems(["Java", "C#", "Python"])
+        self.cb.currentIndexChanged.connect(self.selectionchange)
+
+        layout.addWidget(self.cb)
+        layout.addWidget(self.lbl)
+        self.setLayout(layout)
+
+    def selectionchange(self, i):
+        self.lbl.setText(self.cb.currentText())
+        self.lbl.adjustSize()
+
+        print("Items in the list are: ")
+        for count in range(self.cb.count()):
+            print("item" + str(count) + "=" + self.cb.itemText(count))
+            print("Current index", i, "selection changed", self.cb.currentText())
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    comboxDemo = ComboxDemo()
+    comboxDemo.show()
+    sys.exit(app.exec_())
