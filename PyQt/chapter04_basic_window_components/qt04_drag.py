1bc891216d38a3f66940bd722d3eaa41adf4a8f4
metanoia1989 <sogaxili@gmail.com>
2020-01-30 20:17:26 +0800

    添加拖拽设置和Clipboard

---
+++ PyQt/chapter04_basic_window_components/qt04_drag.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class Combo(QComboBox):
+    def __init__(self, title, parent):
+        super(Combo, self).__init__(parent)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+        print(e)
+        if e.mimeData().hasText():
+            e.accept()
+        else:
+            e.ignore()
+
+    def dropEvent(self, e):
+        self.addItem(e.mimeData().text())
+
+class Example(QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        lo = QFormLayout()
+        lo.addRow(QLabel("请把左边的文本拖拽到右边的下拉菜单中"))
+
+        edit = QLineEdit()
+        edit.setDragEnabled(True)
+
+        com = Combo("Button", self)
+        lo.addRow(edit, com)
+
+        self.setLayout(lo)
+        self.setWindowTitle("简单拖拽例子")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
