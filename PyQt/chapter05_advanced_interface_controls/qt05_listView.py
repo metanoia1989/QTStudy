851a182f5044e04f4f0f0ef99e5f52606a128d39
metanoia1989 <sogaxili@gmail.com>
2020-01-31 10:51:21 +0800

    add QListView QListWidget

---
+++ PyQt/chapter05_advanced_interface_controls/qt05_listView.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QListView, QMessageBox
+from PyQt5.QtCore import QStringListModel
+
+class ListViewDemo(QWidget):
+    def __init__(self, parent=None):
+        super(ListViewDemo, self).__init__(parent)
+        self.setWindowTitle("QListView 例子")
+        self.resize(300, 270)
+        layout = QVBoxLayout()
+
+        listView = QListView()
+        slm = QStringListModel()
+        self.qList = ["Item 1", "Item 2", "Item 3", "Item 4"]
+        slm.setStringList(self.qList)
+        listView.setModel(slm)
+        listView.clicked.connect(self.clicked)
+
+        layout.addWidget(listView)
+        self.setLayout(layout)
+
+    def clicked(self, qModelIndex):
+        QMessageBox.information(self, "QListView", "你选择了：" + self.qList[qModelIndex.row()])
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = ListViewDemo()
+    win.show()
+    sys.exit(app.exec_())
