8f373e2f6eb3d54b4597a75aa0bf043e932e247d
metanoia1989 <sogaxili@gmail.com>
2020-01-31 16:40:20 +0800

    add QApplication.processEvents

---
+++ PyQt/chapter05_advanced_interface_controls/qt05_freshUI.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QListWidget, QGridLayout
+
+class WinForm(QWidget):
+    def __init__(self, parent=None):
+        super(WinForm, self).__init__(parent)
+        self.setWindowTitle("实时刷新界面例子")
+        self.listFile = QListWidget()
+        self.btnStart = QPushButton("开始")
+
+        layout = QGridLayout(self)
+        layout.addWidget(self.listFile, 0, 0, 1, 2)
+        layout.addWidget(self.btnStart, 1, 1)
+        self.btnStart.clicked.connect(self.slotAdd)
+        self.setLayout(layout)
+
+    def slotAdd(self):
+        for n in range(10):
+            str_n = "File index {0}".format(n)
+            self.listFile.addItem(str_n)
+            QApplication.processEvents()
+            time.sleep(1)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = WinForm()
+    form.show()
+    sys.exit(app.exec_())
+
