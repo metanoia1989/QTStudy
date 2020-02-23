df17f9ca3e795e133732250f7104c7bba28292d1
metanoia1989 <sogaxili@gmail.com>
2020-02-23 14:45:06 +0800

    添加Qt国际化翻译

---
+++ Qt5.9Develop/chapter16Ohter/samp16_1MultiLanguage/sample16_1_cn.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>自定义对话框及其调用</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>表格大小</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>设置表格行数列数</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <source>设置表头</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="78"/>
+        <source>设置表头标题</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>退出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="90"/>
+        <source>退出本软件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <source>定位单元格</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <source>定位到某个单元格</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>中文</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="16"/>
+        <source>确认</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="16"/>
+        <source>确定要退出本程序吗？</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="44"/>
+        <source>当前单元格：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <location filename="mainwindow.cpp" line="91"/>
+        <source>单元格内容：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>当前单元格：%1 行，%2 列</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QWDialogHeaders</name>
+    <message>
+        <location filename="qwdialogheaders.ui" line="19"/>
+        <source>设置表头标题</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialogheaders.ui" line="27"/>
+        <source>表头标题</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialogheaders.ui" line="76"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialogheaders.ui" line="100"/>
+        <source>取消</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QWDialogLocate</name>
+    <message>
+        <location filename="qwdialoglocate.ui" line="19"/>
+        <source>单元格定位与文字设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="37"/>
+        <source>列  号</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="64"/>
+        <source>列增</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="71"/>
+        <source>行  号</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="94"/>
+        <source>行增</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="101"/>
+        <location filename="qwdialoglocate.ui" line="129"/>
+        <source>设定文字</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="153"/>
+        <source>关  闭</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QWDialogSize</name>
+    <message>
+        <location filename="qwdialogsize.ui" line="28"/>
+        <source>表格大小设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="46"/>
+        <source>表格大小</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="52"/>
+        <source>列  数</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="72"/>
+        <source>行  数</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="107"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="130"/>
+        <source>取消</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ Qt5.9Develop/chapter16Ohter/samp16_1MultiLanguage/sample16_1_en.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>自定义对话框及其调用</source>
+        <translation>Custom dialog and its usage</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>表格大小</source>
+        <translation>Table size</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>设置表格行数列数</source>
+        <translation>Row and column count setup</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <source>设置表头</source>
+        <translation>Table header</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="78"/>
+        <source>设置表头标题</source>
+        <translation>Table header setup</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>退出</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="90"/>
+        <source>退出本软件</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <source>定位单元格</source>
+        <translation>Cell Location</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <source>定位到某个单元格</source>
+        <translation>Locate to cell</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>中文</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="16"/>
+        <source>确认</source>
+        <translation>Confirm</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="16"/>
+        <source>确定要退出本程序吗？</source>
+        <translation>Sure to exit?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="44"/>
+        <source>当前单元格：</source>
+        <translation>Current Cell:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <location filename="mainwindow.cpp" line="91"/>
+        <source>单元格内容：</source>
+        <translation>Cell content:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>当前单元格：%1 行，%2 列</source>
+        <translation>Current Cell: %1 row, %2 column</translation>
+    </message>
+</context>
+<context>
+    <name>QWDialogHeaders</name>
+    <message>
+        <location filename="qwdialogheaders.ui" line="19"/>
+        <source>设置表头标题</source>
+        <translation>Table header setup</translation>
+    </message>
+    <message>
+        <location filename="qwdialogheaders.ui" line="27"/>
+        <source>表头标题</source>
+        <translation>Table header</translation>
+    </message>
+    <message>
+        <location filename="qwdialogheaders.ui" line="76"/>
+        <source>确定</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="qwdialogheaders.ui" line="100"/>
+        <source>取消</source>
+        <translation>Cancel</translation>
+    </message>
+</context>
+<context>
+    <name>QWDialogLocate</name>
+    <message>
+        <location filename="qwdialoglocate.ui" line="19"/>
+        <source>单元格定位与文字设置</source>
+        <translation>Cell location</translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="37"/>
+        <source>列  号</source>
+        <translation>column Number</translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="64"/>
+        <source>列增</source>
+        <translation>Increase by column</translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="71"/>
+        <source>行  号</source>
+        <translation>Row number</translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="94"/>
+        <source>行增</source>
+        <translation>Increase by row</translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="101"/>
+        <location filename="qwdialoglocate.ui" line="129"/>
+        <source>设定文字</source>
+        <translation>Cell content</translation>
+    </message>
+    <message>
+        <location filename="qwdialoglocate.ui" line="153"/>
+        <source>关  闭</source>
+        <translation>Close</translation>
+    </message>
+</context>
+<context>
+    <name>QWDialogSize</name>
+    <message>
+        <location filename="qwdialogsize.ui" line="28"/>
+        <source>表格大小设置</source>
+        <translation>Table size</translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="46"/>
+        <source>表格大小</source>
+        <translation>Table size</translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="52"/>
+        <source>列  数</source>
+        <translation>Column Count</translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="72"/>
+        <source>行  数</source>
+        <translation>Row count</translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="107"/>
+        <source>确定</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="qwdialogsize.ui" line="130"/>
+        <source>取消</source>
+        <translation>Cancel</translation>
+    </message>
+</context>
+</TS>
