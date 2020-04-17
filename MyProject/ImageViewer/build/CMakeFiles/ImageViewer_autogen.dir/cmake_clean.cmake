ea12014205ce0429f6af3a0345b2ca692ce23d75
metanoia1989 <sogaxili@gmail.com>
2020-04-17 23:27:55 +0800

    更新忽略文件夹

--- MyProject/ImageViewer/build/CMakeFiles/ImageViewer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ImageViewer_autogen"
-  "ImageViewer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ImageViewer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- MyProject/ImageViewer/build/lib/QtAwesome/CMakeFiles/QtAwesome_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/QtAwesome_autogen"
-  "QtAwesome_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/QtAwesome_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
